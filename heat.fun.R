9c648fa74583c4faf2fc1f09d11b60eb5d6f16b9
Weinberger <daniel.weinberger@yale.edu>
2021-06-10 10:47:29 -0500

    fix bug in H numbering (character to numeric)

--- heat.fun.R
+++ heat.fun.R
@@ -1,20 +1,21 @@
 
-heat.fun <- function(target='piab', cut1=15, cut2=45, cut3 = 47){
-ds1 <- d1.a[target,,]
+heat.fun <- function(data.array=d1.a, target='piab', cut1=15, cut2=45, cut3 = 47){
+ds1 <- data.array[target,,]
 ds1[ds1==9999] <- NA
 
 breakslist<-seq(cut1,cut2,by=1)
 
 weeks<- paste0('Week ', 1:6) 
 
-house <- word(dimnames(ds1)[[1]], 2, sep = "_")
+house <- as.numeric(word(dimnames(ds1)[[1]], 2, sep = "_"))
+sampleID <- as.numeric(word(dimnames(ds1)[[1]], 1, sep = "_"))
 
 #First half
 heat1 <- (pheatmap(ds1[1:cut3,], 
                    cluster_cols = F, 
                    cluster_rows = F, 
                    gaps_row = c(head(as.numeric(cumsum(table(house[1:cut3]))), -1)),
-                   labels_row = all$Sample[1:cut3],
+                   labels_row = sampleID[1:cut3],
                    cellwidth = 9,
                    cellheight = 9,
                    color=colorRampPalette(c('red4','red','darkorange','orange','gold','lightgoldenrod1','white','white'))(length(breakslist)),
@@ -26,7 +27,7 @@ heat2<-pheatmap(ds1[(cut3+1):nrow(ds1),],
                 cluster_cols = F, 
                 cluster_rows = F, 
                 gaps_row = c(head(as.numeric(cumsum(table(house[(cut3+1):nrow(ds1)]))), -1)),
-                labels_row = all$Sample[(cut3+1):nrow(ds1)],
+                labels_row = sampleID[(cut3+1):nrow(ds1)],
                 cellwidth = 9,
                 cellheight = 9,
                 color=colorRampPalette(c('red4','red','darkorange','orange','gold','lightgoldenrod1','white','white'))(length(breakslist)),
