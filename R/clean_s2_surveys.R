b2ab8127357efaace8463e01caf006bef1fd6b7f
weinbergerlab <dweinber@gmail.com>
2022-09-24 22:57:21 -0400

    effect of recent col

--- R/clean_s2_surveys.R
+++ R/clean_s2_surveys.R
@@ -2,7 +2,7 @@
 library(dplyr)
 
 clean_s2_surveys <- function(){
-s2_surv <- read.csv('./Data/confidential/Season_2_Surveys/Uptodat Fortnightly_Wyllie_DATA_2022-07-21_1314 .csv') %>%
+s2_surv <- read.csv('./Data/confidential/Season_2_Surveys/fornightly_DATA_2022-09-19.csv') %>%
   rename(scope_id=participant_id, 
          child_contact=child_contact_2weeks,
          child_contact_u12m=child_contact_age___1,
@@ -16,7 +16,7 @@ s2_surv <- read.csv('./Data/confidential/Season_2_Surveys/Uptodat Fortnightly_Wy
          acitivity_fitness=activities_describe___4,
          activity_other=activities_describe___5
          ) %>%
-  mutate(ID = paste0('S2_',gsub("\\..*","",scope_id)) , time=visit_number)
+  mutate(ID = paste0('S2_',gsub("\\..*","",floor(scope_id))) , time=visit_number)
 
 table(s2_surv$visit_number)
 
